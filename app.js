01f567064830a4587108e87cccbece1d126c22e3
bob dylan <823930335@qq.com>
2017-03-24 12:22:21 +0800

    修改0324

---
+++ app.js
@@ -0,0 +1,19 @@
+require.config({
+  baseUrl:'/',
+  paths:{
+    
+    router:'lib/router.min',
+    myroute:'src/myroute',
+    jquery:'lib/jquery-3.1.1',
+    text:'lib/text',
+    css:'lib/css.min'
+
+  }
+})
+
+// 程序入口
+require(['myroute'],function(){
+
+    location.hash = '#/car'
+
+})
