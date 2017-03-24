c8ab9a14764525c70d600cc564e737234221d66b
bob dylan <823930335@qq.com>
2017-03-24 12:24:24 +0800

    修改2

---
+++ server.js
@@ -0,0 +1,25 @@
+var express=require('express');
+var fs=require('fs');
+var app=express();
+
+// 静态文件路径
+app.use(express.static('./'))
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/index.html')
+})
+
+
+app.get('/gethf',function(req,res){
+    fs.readFile(__dirname+'/public/data/sj.json',function(err,data){
+        if(err){console.log("err")}else{
+            var json=JSON.parse(data);
+        }
+    })
+})
+
+
+app.listen(3009,function(){
+    console.log("localhost:3009")
+
+})
